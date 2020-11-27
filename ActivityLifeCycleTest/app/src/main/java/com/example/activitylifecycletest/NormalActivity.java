3981252dfb7f53b3bc314a688503ea949d04d2d9
Deng <zulan.deng@CIENET-PB.com>
2020-11-27 14:51:13 +0800

    Add the "android:configChanges".

--- ActivityLifeCycleTest/app/src/main/java/com/example/activitylifecycletest/NormalActivity.java
+++ ActivityLifeCycleTest/app/src/main/java/com/example/activitylifecycletest/NormalActivity.java
@@ -1,14 +1,44 @@
 package com.example.activitylifecycletest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class NormalActivity extends AppCompatActivity {
+
+    private static final String TAG = NormalActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.normal_layout);
+
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        // Checks the orientation of the screen
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show();
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+            Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d(TAG, "onDestroy");
     }
 }
